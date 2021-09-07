5b2a8565160cc0a44c943f458d3d441f8413c82e
Maria Lacayo <mg.lacayo07@gmail.com>
2021-09-06 23:09:41 -0500

    admin view models

--- appEmpresaDjango/models.py
+++ appEmpresaDjango/models.py
@@ -4,11 +4,15 @@ class Departamento(models.Model):
  # No es necesario crear un campo para la Primary Key, Django creará automáticamente un IntegerField.
  nombre = models.CharField(max_length=50)
  telefono = models.IntegerField()
+ def __str__(self):
+    return self.nombre
 
 class Habilidad(models.Model):
  # No es necesario crear un campo para la Primary Key, Django creará automáticamente un IntegerField.
  nombre = models.CharField(max_length=50)
- 
+ def __str__(self):
+    return self.nombre
+
 class Empleado(models.Model):
  # Campo para la relación one-to-many
  departamento = models.ForeignKey(Departamento, on_delete=models.CASCADE)
@@ -18,3 +22,6 @@ class Empleado(models.Model):
  # Es posible indicar un valor por defecto mediante 'default'
  antiguedad = models.IntegerField(default=0)
  # Para permitir propiedades con valor null, añadiremos las opciones null=True, blank=True.     
+
+ def __str__(self):
+     return self.nombre
