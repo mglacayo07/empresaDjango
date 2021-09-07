0ddf6daf9180294b4cb38ad668932287c584c435
Maria Lacayo <mg.lacayo07@gmail.com>
2021-09-06 22:51:16 -0500

    first app

--- empresaDjango/settings.py
+++ empresaDjango/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'appEmpresaDjango',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
