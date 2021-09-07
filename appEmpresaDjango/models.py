0169f1a6655a89b16613caa70a7af44b306b88b7
Maria Lacayo <mg.lacayo07@gmail.com>
2021-09-06 22:57:26 -0500

    models

--- appEmpresaDjango/models.py
+++ appEmpresaDjango/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Departamento(models.Model):
+ # No es necesario crear un campo para la Primary Key, Django creará automáticamente un IntegerField.
+ nombre = models.CharField(max_length=50)
+ telefono = models.IntegerField()
+
+class Habilidad(models.Model):
+ # No es necesario crear un campo para la Primary Key, Django creará automáticamente un IntegerField.
+ nombre = models.CharField(max_length=50)
+ 
+class Empleado(models.Model):
+ # Campo para la relación one-to-many
+ departamento = models.ForeignKey(Departamento, on_delete=models.CASCADE)
+ habilidades = models.ManyToManyField(Habilidad)
+ nombre = models.CharField(max_length=40)
+ fecha_nacimiento = models.DateField()
+ # Es posible indicar un valor por defecto mediante 'default'
+ antiguedad = models.IntegerField(default=0)
+ # Para permitir propiedades con valor null, añadiremos las opciones null=True, blank=True.     
